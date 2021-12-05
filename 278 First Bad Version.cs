69a0851bebadbc3fab9ac5778f395615f1ef7bb1
eevarast <93538038+eevarast@users.noreply.github.com>
2021-12-05 14:05:24 +0300

    Update 278 First Bad Version.cs

--- 278 First Bad Version.cs
+++ 278 First Bad Version.cs
@@ -1,10 +1,26 @@
 /* The isBadVersion API is defined in the parent class VersionControl.
       bool IsBadVersion(int version); */
-
+//O(n)
+/* 
 public class Solution : VersionControl {
     public int FirstBadVersion(int n) {
         int i = 1;
         while (!IsBadVersion(i)) i++;
         return i;
+    }
+}
+*/
+public class Solution : VersionControl {
+    public int FirstBadVersion(int n) {
+        int left = 1, right = n, center;
+        
+        while (left < right) {
+            center = left + (right - left) / 2;
+            if (IsBadVersion(center)) {
+                right = center;
+            }
+            else { left = center + 1; }
+        }
+        return left;
     }
 }
