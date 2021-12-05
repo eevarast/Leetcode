454bfd2c1640e053cc67346b292d2eb6421b7732
eevarast <93538038+eevarast@users.noreply.github.com>
2021-12-05 13:51:39 +0300

    Create 704 Binary Search.cs

---
+++ 704 Binary Search.cs
@@ -0,0 +1,27 @@
+/*
+Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. 
+If target exists, then return its index. Otherwise, return -1.
+
+You must write an algorithm with O(log n) runtime complexity.
+*/
+
+public class Solution {
+    public int Search(int[] nums, int target) {
+        int left = 0, right = nums.Length - 1; int center; int index = -1;
+        bool isFound = false;
+        
+        while (left <= right) {
+            center = left + (right - left) / 2;
+            if (target == nums[center]) {
+                return center;
+            }
+            else if (target > nums[center]) {
+                left = center + 1;                 
+            }
+            else if (target < nums[center]) {
+                right = center - 1;
+            }
+        }
+        return -1;
+    }
+}
