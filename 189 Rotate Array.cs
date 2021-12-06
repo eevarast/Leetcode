c15aecda101e25781a180a2b60860d3e60f75854
eevarast <93538038+eevarast@users.noreply.github.com>
2021-12-06 11:55:42 +0300

    Create 189 Rotate Array.cs

---
+++ 189 Rotate Array.cs
@@ -0,0 +1,19 @@
+/*
+Given an array, rotate the array to the right by k steps, where k is non-negative.
+*/
+
+public class Solution {
+    public void Rotate(int[] nums, int k) {
+        int[] res = new int[nums.Length];
+        
+        if (k > nums.Length) k = k % nums.Length;
+            
+            for (int i = 0; i < nums.Length; i++)
+            {
+                res[i] = nums[(nums.Length - k + i) % nums.Length];
+            }
+
+            for (int i = 0; i < nums.Length; i++)
+                nums[i] = res[i];
+        }
+    }
