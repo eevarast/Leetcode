26699c528c8561fa3cf9bc6627636db3a7d2ea58
eevarast <93538038+eevarast@users.noreply.github.com>
2021-12-09 10:14:59 +0300

    Create 876 Middle of the Linked List.cs

---
+++ 876 Middle of the Linked List.cs
@@ -0,0 +1,30 @@
+/*
+Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+ 
+ // 80.83%
+public class Solution {
+    public ListNode MiddleNode(ListNode head) {
+        ListNode slow = head; ListNode fast = head;
+            while (fast != null && fast.next != null)
+            {
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+            return slow;
+    }
+}
